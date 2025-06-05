4e2f252793fdd59bbcc6cd9ed7cec3efc1351260
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2025-06-05 09:48:19 +0200

    changed logging to info logging

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             EnvFilter::builder()
-                .with_default_directive(LevelFilter::TRACE.into())
+                .with_default_directive(LevelFilter::INFO.into())
                 .from_env_lossy(),
         )
         .with(fmt::layer())
